fe7f2b722d9862177f4ce51142b9b507fec37bd0
TRKSasank <30754883+TRKSasank@users.noreply.github.com>
2020-06-24 19:12:37 -0500

    Delete Factorial Trailing Zeroes.py

--- Factorial Trailing Zeroes.py
+++
@@ -1,8 +0,0 @@
-import math 
-class Solution:
-    def trailingZeroes(self, n: int) -> int:
-        count = 0
-        for i in list(range(1,len(str(n))+100,1)):
-            count = count + int(n/(5 ** i)) 
-        return count
-                
